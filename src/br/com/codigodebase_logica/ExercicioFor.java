a8c52714d92f475da3a212a7ac8b5c96ec565af3
spjean <spereirajean@outlook.com>
2025-03-17 21:35:44 -0300

    Exercício da função while

---
+++ src/br/com/codigodebase_logica/ExercicioFor.java
@@ -0,0 +1,18 @@
+package br.com.codigodebase_logica;
+
+import java.util.Scanner;
+
+public class ExercicioFor {
+    public static void main(String[] args) {
+        String nome;
+        float nota, media;
+
+        Scanner input = new Scanner (System.in);
+
+        System.out.println("Digite seu nome");
+        nome = input.nextLine();
+
+
+
+    }
+}
